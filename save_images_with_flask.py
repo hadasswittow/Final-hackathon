1f00c4f135090c7d425d9a2692db70571fec5376
hadasswittow <hadasswittow@gmail.com>
2019-12-30 19:00:13 +0200

    flask updated to save cropped images also

--- save_images_with_flask.py
+++ save_images_with_flask.py
@@ -15,14 +15,18 @@ app.config['MONGO_SETTINGS'] = {
 def upload_image():
     jsn = request.get_json()
     img = jsn['img']
+    cropped = jsn['cropped']
+    img_name = jsn['img_name']
+    img_name += '.jpg'
     imgdata = base64.b64decode(img)
-    filename = 'some_image.jpg'
-    with open(f'images/{filename}', 'wb') as f:
+    crpdata =  base64.b64decode(cropped)
+    with open(f'images/{img_name}', 'wb') as f,open(f'cropped/{img_name}', 'wb') as c:
         f.write(imgdata)
+        c.write(crpdata)
     return "got the image!"
 
 
 
 
 if __name__ == "__main__":
-   app.run(host='10.144.67.64')
+   app.run(host='10.144.68.64')
