42345af0677ef674a426be3adacd618d646a19f5
hadasswittow <hadasswittow@gmail.com>
2019-12-31 02:31:41 +0200

    detection of crossing line- code that runs on raspberry

---
+++ openCVCrossingLineDetection/Blob.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+class Blob {
+public:
+	// member variables
+	std::vector<cv::Point> currentContour;
+	cv::Rect currentBoundingRect;
+	std::vector<cv::Point> centerPositions;
+	double dblCurrentDiagonalSize;
+	double dblCurrentAspectRatio;
+	bool blnCurrentMatchFoundOrNewBlob;
+	bool blnStillBeingTracked;
+	int intNumOfConsecutiveFramesWithoutAMatch;
+	cv::Point predictedNextPosition;
+	bool passedLine;
+	bool blobSaved;
+	// function prototypes
+	Blob(std::vector<cv::Point> _contour);
+	void predictNextPosition(void);
+};
